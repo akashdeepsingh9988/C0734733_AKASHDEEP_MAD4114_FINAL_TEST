b3cc5d7f411d54706d0ec543dd6506161c44e671
Akashdeep Singh <akashthind007@gmail.com>
2018-11-22 11:20:41 -0500

    reservation added

--- jenelleteaches-inclassexamplesstarter-afe65986cfcb/InClassExercisesStarter/MakeReservationViewController.swift
+++ jenelleteaches-inclassexamplesstarter-afe65986cfcb/InClassExercisesStarter/MakeReservationViewController.swift
@@ -1,37 +1,35 @@
-//
-//  MakeReservationViewController.swift
-//  InClassExercisesStarter
-//
-//  Created by parrot on 2018-11-22.
-//  Copyright © 2018 room1. All rights reserved.
-//
+// make reservation
 
 import UIKit
 import FirebaseFirestore
 
 
 class MakeReservationViewController: UIViewController {
-
+    
     // MARK: Outlets
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var dayTextField: UITextField!
     @IBOutlet weak var seatsTextField: UITextField!
     
+    @IBOutlet weak var lblResult: UILabel!
     // Mark: Firestore variables
     var db:Firestore!
-    
+    var bc = ""
     
     // MARK: Default Functions
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        
+        bc = UserDefaults.standard.string(forKey: "userId")!
+        print(bc)
         db = Firestore.firestore()
         
         let settings = db.settings
         settings.areTimestampsInSnapshotsEnabled = true
         db.settings = settings
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -41,19 +39,28 @@ class MakeReservationViewController: UIViewController {
     // MARK: Actions
     @IBAction func buttonPressed(_ sender: Any) {
         print("pressed the button")
+        
+        let res = db.collection("reservations")
+        res.document().setData(["username":bc,
+                                "restaurant":nameTextField.text!,
+                                "day":dayTextField.text!,
+                                "numSeats":seatsTextField.text!
+            ])
+        lblResult.text = "Reservation Successfull.  Go back and see the reservations menu"
+        
+        
     }
     
     
     
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
